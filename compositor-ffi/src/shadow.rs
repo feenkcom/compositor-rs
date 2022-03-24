ac03485aaf526d99a7d77237b9add7d15b948e4d
Aliaksei Syrel <alex.syrel@gmail.com>
2022-03-24 22:29:39 +0100

    fix ffi shadow creation

--- compositor-ffi/src/shadow.rs
+++ compositor-ffi/src/shadow.rs
@@ -1,6 +1,5 @@
 use boxer::{ValueBox, ValueBoxPointer, ValueBoxPointerReference};
-use compositor::{Color, Geometry, Layer, Point, Radius, Shadow, ShadowLayer};
-use std::sync::Arc;
+use compositor::{Color, Geometry, Point, Radius, Shadow};
 
 /// Creates a new shadow consuming the geometry
 #[no_mangle]
@@ -11,7 +10,7 @@ pub fn compositor_shadow_new(
     delta_x: f32,
     delta_y: f32,
     mut geometry: *mut ValueBox<Geometry>,
-) -> *mut ValueBox<Arc<dyn Layer>> {
+) -> *mut ValueBox<Shadow> {
     geometry.with_not_null_value_consumed_return(std::ptr::null_mut(), |geometry| {
         let shadow = Shadow::new(
             Color::from_argb(argb),
@@ -19,7 +18,7 @@ pub fn compositor_shadow_new(
             Point::new_f32(delta_x, delta_y),
             geometry,
         );
-        ValueBox::new(Arc::new(ShadowLayer::new(shadow)) as Arc<dyn Layer>).into_raw()
+        ValueBox::new(shadow).into_raw()
     })
 }
 
